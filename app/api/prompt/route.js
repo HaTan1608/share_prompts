34c1ea18ca894e8cb164db4c203a7d60ab45c52a
haphong463 <111565442+haphong463@users.noreply.github.com>
2023-05-27 19:00:45 +0700

    'ok'

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,8 +4,7 @@ import { connectToDB } from "@utils/database";
 export const GET = async (request) => {
     try {
         await connectToDB()
-        const prompts = await Prompt.populate("creator")
-        console.log(prompts)
+        const prompts = await Prompt.find({}).populate("creator")
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
